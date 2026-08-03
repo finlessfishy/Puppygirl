6ba1210586f540510a50834581fc37107beff5cc
finlessfishy <finlessfishy@gmail.com>
2026-08-03 17:20:16 -0500

    Added Python and Lua code execution safety for users

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-local version = "0.9.0"
+local version = "0.9.1"
 
 local tokenizer = require("tokenizer")
 local parser = require("parser")
@@ -20,6 +20,32 @@ local function run_code(code, intro)
 
 
 	local tokens = tokenizer.tokenize(code)
+
+	local has_lua = false
+	local has_python = false
+
+	for _, token in ipairs(tokens) do
+	    if token.type == "KEYWORD_RUN_LUA" then
+	        has_lua = true
+	    elseif token.type == "KEYWORD_RUN_PYTHON" then
+	        has_python = true
+	    end
+	end
+
+	if has_lua or has_python then
+		print()
+	    if has_lua then
+	    	print("\nWait!! There is Lua code execution in this game, so it might do dangerous things to your computer if the developer has bad intentions!")
+	    	io.write("\nPress enter to continue, but only if you trust the developer!!!\n")
+	    	io.read()
+	    end
+	    if has_python then
+	    	print("\nWait!! There is Python code execution in this game, so it might do dangerous things to your computer if the developer has bad intentions!")
+	    	io.write("\nPress enter to continue, but only if you trust the developer!!!\n")
+	    	io.read()
+	    end
+	end
+
 	local ast = parser.parse(tokens)
 	local env = interpreter.run(ast)
 
