033d3795cd44ff446b59a6ff96e020de7059643b
finlessfishy <finlessfishy@gmail.com>
2026-08-03 12:52:00 -0500

    added helper functions to parser

--- parser.lua
+++ parser.lua
@@ -1,6 +1,23 @@
 
 
 
-function parse(string)
-	
+function parse(tokens)
+	local cursor = 1
+
+	local function peek()
+		return tokens[cursor]
+	end
+
+	local function advance()
+		cursor += 1
+		return cursor - 1
+	end
+
+	local function match(expected_type)
+		if expected_type == peek() then
+			return advance()
+		else
+			print("SYNTAX ERROR (match function)")
+		end
+	end
 end
